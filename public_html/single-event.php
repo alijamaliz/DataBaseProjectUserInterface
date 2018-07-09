d6039e8bc61887fe77343cbd2a7780c22194de27
alijamaliz <jamaliali219@gmail.com>
2018-07-10 02:09:08 +0430

    Implement single event page

---
+++ public_html/single-event.php
@@ -0,0 +1,142 @@
+<?php
+
+require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+require_once(realpath(dirname(__FILE__) . "/../resources/queries.php"));
+
+require_once(TEMPLATES_PATH . "/header.php");
+
+$connection = new mysqli($config[db][host], $config[db][username], $config[db][password], $config[db][dbname]);
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$id = $_GET['id'];
+
+$event = $connection->query(get_event_by_id($id));
+$comments = $connection->query(get_event_comments_by_id($id));
+$hashtags = $connection->query(get_event_hashtags_by_id($id));
+$participants = $connection->query(get_event_participants_by_id($id));
+
+?>
+
+    <h1>جزییات رویداد</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">تا تاریخ</th>
+            <th scope="col">از تاریخ</th>
+            <th scope="col">برگزارکننده</th>
+            <th scope="col">مکان</th>
+            <th scope="col">موضوع</th>
+            <th scope="col">عنوان</th>
+            <th scope="col">#</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        if ($event->num_rows == 1) {
+            // output data of each row
+            while ($row = $event->fetch_assoc()) {
+                echo sprintf('<tr><th scope="row">%d</th><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%d</td></tr>',
+                    $row["date_to"], $row["date_from"], $row["holder_name"], $row["location_title"], $row["subject_title"], $row["event_title"], $row["event_id"]);
+            }
+        } else {
+            echo "Not found";
+        }
+        ?>
+
+        </tbody>
+    </table>
+
+
+    <h1>برچسب‌ها</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">برچسب</th>
+            <th scope="col">#</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        if ($hashtags->num_rows > 0) {
+            // output data of each row
+            while ($row = $hashtags->fetch_assoc()) {
+                echo sprintf('<tr><th scope="row">%s</th><td>%d</td>', $row["hashtag_text"], $row["hashtag_id"]);
+            }
+        } else {
+            echo "Not found";
+        }
+        ?>
+
+        </tbody>
+    </table>
+
+
+    <h1>نظرات</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">نظر</th>
+            <th scope="col">کاربر</th>
+            <th scope="col">#</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        if ($comments->num_rows > 0) {
+            // output data of each row
+            while ($row = $comments->fetch_assoc()) {
+                echo sprintf('<tr><th scope="row">%s</th><td>%s</td><td>%d</td>', $row["comment_text"], $row["particicpant_name"], $row["comment_id"]);
+            }
+        } else {
+            echo "Not found";
+        }
+        ?>
+
+        </tbody>
+    </table>
+
+
+    <h1>شرکت‌کننده‌ها</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">شماره‌ملی</th>
+            <th scope="col">شماره‌تلفن</th>
+            <th scope="col">جنسیت</th>
+            <th scope="col">نام</th>
+            <th scope="col">#</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php
+        if ($participants->num_rows > 0) {
+            $i = 1;
+            // output data of each row
+            while ($row = $participants->fetch_assoc()) {
+                $gender = "مرد";
+                if ($row["participant_gender"] == 0)
+                    $gender = "زن";
+                echo sprintf('<tr><th scope="row">%s</th><td>%s</td><td>%s</td><td>%s</td><td>%d</td>', $row["participant_national_code"], $row["participant_number"], $gender, $row["participant_name"], $i);
+                $i++;
+            }
+        } else {
+            echo "Not found";
+        }
+        ?>
+
+        </tbody>
+    </table>
+
+<?php
+$connection->close();
+?>
+
+<?php
+require_once(TEMPLATES_PATH . "/footer.php");
+?>
--- resources/queries.php
+++ resources/queries.php
@@ -29,3 +29,43 @@ FROM `event` JOIN `subject` JOIN `location` JOIN `event_holder` JOIN `user`
 WHERE `location`.`id` = `event`.`location_id` AND `subject`.`id` = `event`.`subject_id` AND 
   `event`.`holder_national_code` = `event_holder`.`user_national_code` AND 
   `event_holder`.`user_national_code` = `user`.`national_code`";
+
+
+function get_event_by_id($id)
+{
+    return "
+SELECT event.id as event_id,
+    event.title as event_title,
+    subject.text as subject_title,
+    location.title as location_title,
+    CONCAT(user.first_name, ' ', user.last_name) as holder_name,
+    event.date_from as date_from,
+    event.date_to as date_to
+FROM `event` JOIN `subject` JOIN `location` JOIN `event_holder` JOIN `user`
+WHERE `event`.`id` = $id AND `location`.`id` = `event`.`location_id` AND `subject`.`id` = `event`.`subject_id` AND 
+  `event`.`holder_national_code` = `event_holder`.`user_national_code` AND 
+  `event_holder`.`user_national_code` = `user`.`national_code`";
+}
+
+
+function get_event_comments_by_id($id)
+{
+    return "SELECT comment.id as comment_id, comment.text as comment_text, concat(user.first_name, ' ', user.last_name) as particicpant_name
+            FROM `user` JOIN event_comment JOIN comment WHERE event_id = $id AND 
+            event_comment.comment_id = comment.id AND event_comment.participant_national_code = user.national_code";
+}
+
+function get_event_hashtags_by_id($id)
+{
+    return "SELECT hashtag.id as hashtag_id, hashtag.text as hashtag_text 
+            FROM event_hashtag JOIN hashtag WHERE event_id = $id AND event_hashtag.hashtag_id = hashtag.id";
+}
+
+function get_event_participants_by_id($id)
+{
+    return "SELECT concat(`user`.first_name, ' ', `user`.last_name) as participant_name, `participant`.gender as participant_gender, 
+                  `user`.mobile_number as participant_number, `user`.national_code as participant_national_code
+            FROM `participant` JOIN `participate` JOIN `user` 
+            WHERE event_id = $id AND participant.user_national_code = participate.participant_national_code 
+            AND participant.user_national_code = `user`.national_code";
+}
