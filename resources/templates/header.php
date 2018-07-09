da1e32fbf2ee80b30913e15d927505c748be0c1c
alijamaliz <jamaliali219@gmail.com>
2018-07-10 02:07:57 +0430

    Amend header.php

---
+++ resources/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.rtlcss.com/bootstrap/v4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <title>DB Project</title>
+</head>
+
+<body>
+<nav class="navbar navbar-dark bg-dark">
+    <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="all-events.php">همه‌رویدادها<span class="sr-only">(current)</span></a>
+        </li>
+<!--        <li class="nav-item">-->
+<!--            <a class="nav-link" href="#">Features</a>-->
+<!--        </li>-->
+<!--        <li class="nav-item">-->
+<!--            <a class="nav-link" href="#">Pricing</a>-->
+<!--        </li>-->
+    </ul>
+</nav>
