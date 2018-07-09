2a42176b8faf662dde0bf45d4e2a04bdbe1ed844
alijamaliz <jamaliali219@gmail.com>
2018-07-10 00:29:08 +0430

    Implement config file

---
+++ resources/config.php
@@ -0,0 +1,33 @@
+<?php
+$config = array(
+    "db" => array(
+        "dbname" => "eventor",
+        "username" => "root",
+        "password" => "root",
+        "host" => "localhost"
+    ),
+    "urls" => array(
+        "baseUrl" => "http://example.com"
+    ),
+    "paths" => array(
+        "resources" => "/path/to/resources",
+        "images" => array(
+            "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+            "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+        )
+    )
+);
+
+defined("LIBRARY_PATH")
+or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+
+defined("TEMPLATES_PATH")
+or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+
+/*
+    Error reporting.
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL | E_STRCT);
+
+?>
