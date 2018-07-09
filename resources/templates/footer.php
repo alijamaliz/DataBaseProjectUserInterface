50c51aea4386e41bbe98b4e35fa0ee57c64a897f
alijamaliz <jamaliali219@gmail.com>
2018-07-10 03:15:29 +0430

    Ament footer text

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
-    <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+    <div class="container" style="text-align: center;">
+        <span class="text-muted">پروژه پایانی درس پایگاه داده، ترم بهار ۹۷<br>دانشگاه علم و صنعت ایران</span>
     </div>
 </footer>
 </body>
